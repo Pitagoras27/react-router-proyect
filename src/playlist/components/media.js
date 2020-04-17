311b357b6298cf9999010a4996a4426cbd252020
Carlos Gutierrez <elimperiodeloefimero@gmail.com>
2020-04-16 21:31:22 -0500

    5. Link component permite gestionar lo q se visualiza en la dirección electrónica del navegador con su prop pathname y con search agregar un queryString

--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -1,5 +1,7 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
 import './media.css';
 
 class Media extends PureComponent {
@@ -30,25 +32,36 @@ class Media extends PureComponent {
       }
     }
     return (
-      <div className="Media" onClick={this.handleClick}>
-        <div className="Media-cover">
-          <img
-            src={this.props.cover}
-            alt=""
-            width={260}
-            height={160}
-            className="Media-image"
-          />
+      <Link
+        to={{
+          pathname: '/videos',
+          search: `?id=${this.props.id}`,
+          state: {
+            modal: true,
+            id: this.props.id
+          }
+        }}
+      >
+        <div className="Media" onClick={this.handleClick}>
+          <div className="Media-cover">
+            <img
+              src={this.props.cover}
+              alt=""
+              width={260}
+              height={160}
+              className="Media-image"
+            />
+          </div>
+          <h3 className="Media-title">{this.props.title}</h3>
+          <p className="Media-author">{this.props.author}</p>
         </div>
-        <h3 className="Media-title">{this.props.title}</h3>
-        <p className="Media-author">{this.props.author}</p>
-      </div>
+      </Link>
     )
   }
 }
 
 Media.propTypes = {
-  cover: PropTypes.string,
+  cover: PropTypes.string,  
   title: PropTypes.string.isRequired,
   author: PropTypes.string,
   type: PropTypes.oneOf(['video', 'audio']),
