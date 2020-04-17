8b0b2bccbde572c3173930f9997cfbdd9faefd9b
Carlos Gutierrez <elimperiodeloefimero@gmail.com>
2020-04-16 21:39:50 -0500

    5. Se agrega el pathname al componnete modal para limpiar la url al cerrar el modal

--- src/widgets/components/modal.js
+++ src/widgets/components/modal.js
@@ -1,15 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import './modal.css';
 
 function Modal(props) {
   return (
-    <div className="Modal">
-      {props.children}
-      <button
-        onClick={props.handleClick}
-        className="Modal-close"
-      />
-    </div>
+    <Link
+      to={{
+        pathname:'/videos',
+      }}
+    >
+      <div className="Modal">
+        {props.children}
+        <button
+          onClick={props.handleClick}
+          className="Modal-close"
+        />
+      </div>
+    </Link>
   )
 }
 
