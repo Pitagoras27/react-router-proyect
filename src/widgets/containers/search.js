c0d976ab6dbc96a1326c5448d392705c2e327374
Carlos Gutierrez <elimperiodeloefimero@gmail.com>
2020-05-14 17:01:21 -0500

    Se agrega componente Prompt de react-router que avisa al usuario si no a terminado una accion iniciada

--- src/widgets/containers/search.js
+++ src/widgets/containers/search.js
@@ -6,7 +6,8 @@ import { bindActionCreators } from 'redux';
 
 class SearchContainer extends Component {
   state = {
-    value: 'Luis Fonsi'
+    value: '',
+    prompt: false,
   }
   handleSubmit = event => {
     event.preventDefault();
@@ -20,16 +21,19 @@ class SearchContainer extends Component {
   }
   handleInputChange = event => {
     this.setState({
-      value: event.target.value.replace(' ', '-')
-    })
+      value: event.target.value.replace(' ', '-'),
+      prompt: !!(event.target.value.length),
+    });
   }
   render() {
+    const { prompt } = this.state;
     return (
       <Search
         setRef={this.setInputRef}
         handleSubmit={this.handleSubmit}
         handleChange={this.handleInputChange}
         value={this.state.value}
+        prompt={prompt}
       />
     )
   }
