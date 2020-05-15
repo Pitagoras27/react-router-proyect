7016d946b4cd97cf9969f8b2fb5e4a2337343d7b
Carlos Gutierrez <elimperiodeloefimero@gmail.com>
2020-05-14 19:32:08 -0500

    Se agrega HOC de react-router para pasarle las propiedades history, location y match en un componente que no esté dentro del componente Route.

--- src/pages/components/header.js
+++ src/pages/components/header.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 import './header.css';
 import { Link, NavLink } from 'react-router-dom';
+import { withRouter } from 'react-router';
 import logo from '../../../images/logo.png';
+import HandleError from '../../error/containers/handle-error';
 
 class Header extends Component {
+  handleClick = () => {
+    this.props.history.goBack()
+  }
   render() {
     return (
       <header className="Header">
@@ -35,6 +40,11 @@ class Header extends Component {
                 Redirect
               </NavLink>
             </li>
+            <li>
+              <a onClick={this.handleClick}>
+                Go Back
+              </a>
+            </li>
           </ul>
         </nav>
       </header>
@@ -42,4 +52,4 @@ class Header extends Component {
   }
 }
 
-export default Header
+export default withRouter(Header)
