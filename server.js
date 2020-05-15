44dd13da13c1d377545ef26692749214f9b238ea
Carlos Gutierrez <elimperiodeloefimero@gmail.com>
2020-05-15 14:33:21 -0500

    Se pasa el staticRouter al callback del metodo get de express para pasar la ruta dinamica a la prop location, el prop context permite pasar un objeto con nombre valor a la app

--- server.js
+++ server.js
@@ -5,15 +5,19 @@ import { StaticRouter } from 'react-router/';
 import reactDOMServer from 'react-dom/server';
 
 var app = express();
-const html = reactDOMServer.renderToString(
-  <StaticRouter>
-    <App />
-  </StaticRouter>
-);
 
-app.get('/', (req, res) => {
-  console.log(req.url);
-  res.write('works')
+app.get('*', (req, res) => {
+  const html = reactDOMServer.renderToString(
+    <StaticRouter
+      location={req.url}
+      context={{
+        name:'variables of server to app'
+      }}
+    >
+      <App />
+    </StaticRouter>
+  );
+  res.write(`works ${req.url}`)
   res.end();
 })
 
